f03b7f12451ad4f241d52194ad2eb951c61b2ade
noceive <nocivescience@gmail.com>
2022-07-29 23:54:45 -0400

    terminado pero que se puede mejorar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const app=require('express')()
-app.set('port', process.env.PORT||3000)
+const express=require('express');
+const app=express();
+const {engine}=require('express-handlebars');
+const morgan=require('morgan');
+const path=require('path')
+const indexRouter=require('./routers/index.routers')
+app.set('port', process.env.PORT||3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine(
+    '.hbs',
+    engine({
+        defaultLayout:'main',
+        layoutsDir:path.join(app.get('views'),'layouts'),
+        partialsDir: path.join(app.get('views'),'partials'),
+        extname:'.hbs'
+    })
+);
+app.set('view engine', '.hbs');
+app.use(indexRouter)
 app.listen(app.get('port'),()=>{
     console.log(`puerto ${app.get('port')} listo`)
 })
---
+++ src/routers/index.routers.js
@@ -0,0 +1,5 @@
+const {Router}=require('express');
+const {renderIndex}=require('../controllers/index.controllers')
+const router=Router();
+router.get('/',renderIndex);
+module.exports=router;
